39500ff2796c6214d82769c57ad681b94e20bcc2
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-10 20:56:56 -0700

    Add subprocess module

--- netfluff.py
+++ netfluff.py
@@ -12,24 +12,6 @@ target             = ""
 upload_destination = ""
 port               = 0
 
-def usage():
-    print "NetFluff Tool"
-    print
-    print "Usage netfluff.py -t target_host -p port"
-    print "-l --listen                    - listen on [host]:[port] for incoming connections"
-    print "-e --execute=file_to_run       - execute the given file upon receiving a connection"
-    print "-c --command                   - initialize a command shell"
-    print "-u --upload=destination        - upon receiving connection upload a file and write to [destination]"
-    print
-    print
-    print "Examples: "
-    print "netfluff.py -t 192.168.0.1 -p 5555 -l -c"
-    print "netfluff.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
-    print "netfluff.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
-    print "echo 'ABCDEFG' | ./netfluff.py -t 192.168.11.12 -p 135"
-    sys.exit(0)
-
-
 def main():
     global listen
     global command
@@ -54,7 +36,7 @@ def main():
             listen = True
         elif o in ("-e", "--execute"):
             execute = a
-        elif o in ("-c", "--commandshell"):
+        elif o in ("-c", "--command"):
             command = True
         elif o in ("-u", "--upload"):
             upload_destination = a
@@ -70,5 +52,79 @@ def main():
         client_sender(buffer)
 
     if listen: server_loop()
+
+def usage():
+    print "NetFluff Tool"
+    print
+    print "Usage netfluff.py -t target_host -p port"
+    print "-l --listen                    - listen on [host]:[port] for incoming connections"
+    print "-e --execute=file_to_run       - execute the given file upon receiving a connection"
+    print "-c --command                   - initialize a command shell"
+    print "-u --upload=destination        - upon receiving connection upload a file and write to [destination]"
+    print
+    print
+    print "Examples: "
+    print "netfluff.py -t 192.168.0.1 -p 5555 -l -c"
+    print "netfluff.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
+    print "netfluff.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
+    print "echo 'ABCDEFG' | ./netfluff.py -t 192.168.11.12 -p 135"
+    sys.exit(0)
+
+def send_client(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect(target, port)
+
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096: break
+
+            print response,
+            buffer = raw_input("")
+            buffer += "\n"
+
+            client.send(buffer)
+    except:
+        print "[*] Exception! Exiting now."
+        client.close
+
+def server_loop():
+    global target
+
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+
+    server.listen(5)
+
+    while True:
+        client_socket, address = server.accept()
+
+        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+        client_thread.start()
+
+def run_command():
+    command = command.rstrip()
+
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = "Failed to execute command.\r\n"
+
+    return output
+
 
 main()
