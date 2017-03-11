3fdeffa88612afca97a30de9740af0de346f9b1a
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-10 19:42:45 -0700

    Initial commit

---
+++ netfluff.py
@@ -0,0 +1,74 @@
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+
+listen             = False
+command            = False
+upload             = False
+execute            = ""
+target             = ""
+upload_destination = ""
+port               = 0
+
+def usage():
+    print "NetFluff Tool"
+    print
+    print "Usage netfluff.py -t target_host -p port"
+    print "-l --listen                    - listen on [host]:[port] for incoming connections"
+    print "-e --execute=file_to_run       - execute the given file upon receiving a connection"
+    print "-c --command                   - initialize a command shell"
+    print "-u --upload=destination        - upon receiving connection upload a file and write to [destination]"
+    print
+    print
+    print "Examples: "
+    print "netfluff.py -t 192.168.0.1 -p 5555 -l -c"
+    print "netfluff.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
+    print "netfluff.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
+    print "echo 'ABCDEFG' | ./netfluff.py -t 192.168.11.12 -p 135"
+    sys.exit(0)
+
+
+def main():
+    global listen
+    global command
+    global execute
+    global target
+    global upload_destination
+    global port
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu", ["help", "listen", "execute", "target", "port", "command", "upload"])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-c", "--commandshell"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
+
+    if not listen and len(target) and port > 0:
+        buffer = sys.stdin.read()
+        client_sender(buffer)
+
+    if listen: server_loop()
+
+main()
---
+++ tcp-server.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+bind_ip = "127.0.0.1"
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind_port))
+
+server.listen(5)
+
+print "[*] Listening on %s %d" % (bind_ip, bind_port)
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    print "[*] Received %s" % request
+    client_socket.send("MESSAGE!")
+    client_socket.close()
+
+while True:
+    client, address = server.accept()
+    print "[*] Accepted confirmation from %s:%d" % (address[0], address[1])
+
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
